0004662c48836c22fafc860aa3850b2e01f488e6
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-22 16:22:36 +0530

    [Rutuja]:Welcome

---
+++ Obect-Oriented-Practice-Problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Obect_Oriented_Practice_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented Practice Problems");
+        }
+    }
+}
