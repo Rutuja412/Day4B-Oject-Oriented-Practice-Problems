b46c9293e216bef2eec36e2864a7e8a8789ccbdc
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-23 11:42:19 +0530

    [Rutuja]:Adding program to print star pattern

---
+++ Obect-Oriented-Practice-Problems/Pattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obect_Oriented_Practice_Problems
+{
+    internal class Pattern
+    {
+        public static void CreateReverseRightStarPattern()
+        {
+            Console.WriteLine("Enter number of * input");
+            int n =Convert.ToInt32(Console.ReadLine());
+            for (int row = n; row >= 1; row--)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Obect-Oriented-Practice-Problems/Program.cs
+++ Obect-Oriented-Practice-Problems/Program.cs
@@ -7,10 +7,28 @@ namespace Obect_Oriented_Practice_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Object Oriented Practice Problems");
-            //DuplicateNumber.CheckDuplicate();
-            //UniqueElement.CheckUniqueElement();
-            //FrequencyOfElement.FindFrequency();
-            FindMaxMin.FindMaximumMinimum();
+            Console.WriteLine("Select given options");
+            Console.WriteLine("1.Duplicate Element \n2.Unique Element\n3Frequency Of Element\n4 Find Max & Min\n5.Create Reverse Right Star Pattern ");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Duplicate Number");
+                    DuplicateNumber.CheckDuplicate();
+                    break;
+                case 2:
+                    UniqueElement.CheckUniqueElement();
+                    break;
+                case 3:
+                    FrequencyOfElement.FindFrequency();
+                    break;
+                case 4:
+                    FindMaxMin.FindMaximumMinimum();
+                    break;
+                case 5:
+                    Pattern.CreateReverseRightStarPattern();
+                    break;
+            }
         }
     }
 }
